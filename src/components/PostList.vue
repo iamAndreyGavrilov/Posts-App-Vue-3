0f1a168144edd97bf9f4ee30a19ec7d86b5d8981
iamAndreyGavrilov <79842149+iamAndreyGavrilov@users.noreply.github.com>
2022-03-22 18:03:18 +0500

    add remove post

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <h3>Список постов</h3>
-    <post-item v-for="post in posts" :key="post.id" :post="post" />
+    <post-item
+      v-for="post in posts"
+      :key="post.id"
+      :post="post"
+      @removePost="$emit('removePost', post)"
+    />
   </div>
 </template>
 
