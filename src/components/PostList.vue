f1fd74609736189973ee27cc159d83c4aab27fbf
iamAndreyGavrilov <79842149+iamAndreyGavrilov@users.noreply.github.com>
2022-03-25 19:13:09 +0500

    add transition-group

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
   <div v-if="posts.length > 0">
     <h3>Список постов</h3>
-    <post-item
-      v-for="post in posts"
-      :key="post.id"
-      :post="post"
-      @removePost="$emit('removePost', post)"
-    />
+    <transition-group name="post-list">
+      <post-item
+        v-for="post in posts"
+        :key="post.id"
+        :post="post"
+        @removePost="$emit('removePost', post)"
+      />
+    </transition-group>
   </div>
   <h2 v-else>Список постов пуст, напиши что-нибудь!</h2>
 </template>
@@ -24,4 +26,21 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.5s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+.post-list-move {
+  transition: transform 0.5s ease;
+}
+</style>
